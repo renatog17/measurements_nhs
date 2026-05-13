66f0312063f3f3e5f654f3755523d77cabe32c35
renatog17 <renatog.oliveira17@gmail.com>
2026-05-13 04:09:55 -0300

    profiles created

--- src/main/java/com/nhst/medicoes/FlywayCleanRunner.java
+++ src/main/java/com/nhst/medicoes/FlywayCleanRunner.java
@@ -3,9 +3,11 @@ package com.nhst.medicoes;
 import lombok.RequiredArgsConstructor;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("dev")
 @RequiredArgsConstructor
 public class FlywayCleanRunner implements CommandLineRunner {
 
